1db0e2b246166f6effe4c5a1f77cf2a6c92df969
Nitin Ranganath <nitinranganath@gmail.com>
2021-08-23 16:36:23 +0530

    Build post model

---
+++ models/Post.model.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    techStack: {
+      type: [String],
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+    },
+    liveDemo: {
+      type: String,
+      required: true,
+    },
+    sourceCode: {
+      type: String,
+    },
+    likes: [
+      {
+        user: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'User',
+        },
+      },
+    ],
+    comments: [
+      {
+        _id: { type: String, required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        text: { type: String, required: true },
+        date: { type: Date, default: Date.now },
+      },
+    ],
+    views: {
+      type: Number,
+      default: 0,
+    },
+    saves: {
+      type: Number,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Post', postSchema);
