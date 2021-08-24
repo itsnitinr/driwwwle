b55267f7f187af1bec83528048b010b9afad1b03
Nitin Ranganath <nitinranganath@gmail.com>
2021-08-24 21:58:52 +0530

    Fetch posts on home page with react-query

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,14 +1,36 @@
+import axios from 'axios';
+import { QueryClient, useQuery } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
+
+import baseURL from '../utils/baseURL';
 import HeroBanner from '../components/home/HeroBanner';
 
-const HomePage = ({ user }) => {
+const getPosts = async () => {
+  const { data } = await axios.get(`${baseURL}/api/posts`);
+  return data;
+};
+
+const HomePage = () => {
+  const { data } = useQuery(['posts'], getPosts);
+
   return (
     <>
       <HeroBanner />
       <main className="container mx-auto p-8 md:px-16 md:py-10">
-        <h1 className="text-2xl text-pink-600 font-bold">Recent Posts</h1>
+        {data.map((post) => (
+          <h1 key={post._id}>{post.title}</h1>
+        ))}
       </main>
     </>
   );
 };
+
+export async function getServerSideProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(['posts'], getPosts);
+
+  return { props: { dehydratedState: dehydrate(queryClient) } };
+}
 
 export default HomePage;
