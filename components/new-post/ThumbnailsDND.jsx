eee5c951df3ae2cced09a92f4eb9582a08c2624e
Nitin Ranganath <nitinranganath@gmail.com>
2021-08-25 20:23:42 +0530

    Allow sorting thumbnails via drag and drop

---
+++ components/new-post/ThumbnailsDND.jsx
@@ -0,0 +1,54 @@
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const ThumbnailsDND = ({ images, setImages }) => {
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const items = Array.from(images);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+    setImages(items);
+  }
+
+  return (
+    <div className="my-6">
+      <h3 className="text-xl mb-1 font-semibold text-pink-600">
+        Arrange Images
+      </h3>
+      <p className="mb-4 text-gray-500">
+        The first image will be used as the thumbnail
+      </p>
+      <DragDropContext onDragEnd={handleOnDragEnd}>
+        <Droppable droppableId="thumbnails" direction="horizontal">
+          {(provided) => (
+            <div
+              className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3"
+              {...provided.droppableProps}
+              ref={provided.innerRef}
+            >
+              {images.map((image, index) => (
+                <Draggable
+                  key={image.path}
+                  draggableId={image.path}
+                  index={index}
+                >
+                  {(provided) => (
+                    <img
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      src={URL.createObjectURL(image)}
+                      className="w-full h-full rounded-lg shadow cursor-pointer"
+                    />
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
+    </div>
+  );
+};
+
+export default ThumbnailsDND;
--- pages/posts/new.jsx
+++ pages/posts/new.jsx
@@ -1,24 +1,10 @@
-import { useState, useCallback } from 'react';
-import { useDropzone } from 'react-dropzone';
-import { PhotographIcon } from '@heroicons/react/outline';
+import { useState } from 'react';
+
+import ImageDropzone from '../../components/new-post/ImageDropzone';
+import ThumbnailsDND from '../../components/new-post/ThumbnailsDND';
 
 const NewPost = ({ user }) => {
   const [images, setImages] = useState([]);
-
-  const onDrop = useCallback((acceptedFiles) => {
-    setImages(acceptedFiles);
-  }, []);
-
-  const onDropRejected = () => {
-    alert('Please drop upto 5 image files only!');
-  };
-
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    maxFiles: 5,
-    accept: 'image/*',
-    onDropRejected,
-  });
 
   return (
     <div className="bg-pink-50 px-6 md:px-20 xl:px-32 py-10">
@@ -27,38 +13,9 @@ const NewPost = ({ user }) => {
           Ready to post,{' '}
           <span className="text-pink-600">{user.name.split(' ')[0]}</span>?
         </h1>
-        <div
-          className="bg-white cursor-pointer shadow-md rounded-xl p-8 w-full text-center"
-          {...getRootProps()}
-        >
-          <div className="border-2 border-dashed divide-x rounded-md p-10 border-pink-600 flex flex-col justify-center items-center">
-            <input {...getInputProps()} />
-            <PhotographIcon className="w-16 h-16 mb-4 text-pink-600" />
-            <p className="text-lg font-semibold mb-1">
-              Drop your images here or{' '}
-              <span className="text-pink-600">browse</span>
-            </p>
-            <p className="text-md text-gray-400">Maximum 5 image files only</p>
-          </div>
-        </div>
+        <ImageDropzone setImages={setImages} />
         {images.length > 0 && (
-          <div className="my-6">
-            <h3 className="text-xl mb-1 font-semibold text-pink-600">
-              Arrange Images
-            </h3>
-            <p className="mb-4 text-gray-500">
-              The first image will be used as the thumbnail
-            </p>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3">
-              {images.map((image) => (
-                <img
-                  key={image.path}
-                  src={URL.createObjectURL(image)}
-                  className="w-full h-full rounded-lg shadow cursor-pointer"
-                />
-              ))}
-            </div>
-          </div>
+          <ThumbnailsDND images={images} setImages={setImages} />
         )}
       </div>
     </div>
