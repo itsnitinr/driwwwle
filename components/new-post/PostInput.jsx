f78f1b031a32d0ceac4dfef6bd4b9453e9aacc98
Nitin Ranganath <nitinranganath@gmail.com>
2021-08-26 20:16:50 +0530

    Add text inputs to new post page

---
+++ components/new-post/PostInput.jsx
@@ -0,0 +1,40 @@
+const PostInput = ({
+  Icon,
+  label,
+  small,
+  id,
+  placeholder,
+  value,
+  setValue,
+  required,
+}) => {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className="block text-md font-medium text-pink-600 mb-2"
+      >
+        {label}
+        {small && (
+          <small className="block text-xs text-gray-500 mb-3">{small}</small>
+        )}
+      </label>
+      <div className="mt-1 relative rounded-md shadow-sm">
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <Icon className="h-5 w-5 text-pink-600" aria-hidden="true" />
+        </div>
+        <input
+          type="text"
+          id={id}
+          className="focus:ring-pink-500 focus:border-pink-500 block w-full pl-10 sm:text-sm border-gray-300 rounded-md"
+          placeholder={placeholder}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          required={required}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PostInput;
--- pages/posts/new.jsx
+++ pages/posts/new.jsx
@@ -1,18 +1,68 @@
 import { useState } from 'react';
+import {
+  PencilIcon,
+  TerminalIcon,
+  LinkIcon,
+  CodeIcon,
+} from '@heroicons/react/outline';
 
+import PostInput from '../../components/new-post/PostInput';
 import ImageDropzone from '../../components/new-post/ImageDropzone';
 import ThumbnailsDND from '../../components/new-post/ThumbnailsDND';
 
 const NewPost = ({ user }) => {
+  const [title, setTitle] = useState('');
+  const [techStack, setTechStack] = useState('');
+  const [liveDemo, setLiveDemo] = useState('');
+  const [sourceCode, setSourceCode] = useState('');
   const [images, setImages] = useState([]);
 
   return (
     <div className="bg-pink-50 px-6 md:px-20 xl:px-32 py-10">
       <div className="container mx-auto flex flex-col items-center">
-        <h1 className="text-3xl mb-10 text-gray-700 font-bold mr-auto">
+        <h1 className="text-3xl mb-8 text-gray-700 font-bold mr-auto">
           Ready to post,{' '}
           <span className="text-pink-600">{user.name.split(' ')[0]}</span>?
         </h1>
+        <div className="mb-5 mr-auto block w-full">
+          <PostInput
+            Icon={PencilIcon}
+            label="Give your post a title"
+            id="title"
+            placeholder="My new epic website"
+            value={title}
+            setValue={setTitle}
+          />
+        </div>
+        <div className="mb-5 mr-auto block w-full">
+          <PostInput
+            Icon={CodeIcon}
+            small="Please use comma separated values"
+            label="What tech stack did you use?"
+            id="tech-stack"
+            placeholder="Next.js, TailwindCSS, MongoDB, Socket.io"
+            value={techStack}
+            setValue={setTechStack}
+          />
+        </div>
+        <div className="mb-10 mr-auto grid md:grid-cols-2 gap-4 w-full">
+          <PostInput
+            Icon={LinkIcon}
+            label="Deployed website URL"
+            id="live-demo"
+            placeholder="https://nitinranganath.me"
+            value={liveDemo}
+            setValue={setLiveDemo}
+          />
+          <PostInput
+            Icon={TerminalIcon}
+            label="Source code repository"
+            id="source-code"
+            placeholder="https://github.com/itsnitinr/driwwwle"
+            value={sourceCode}
+            setValue={setSourceCode}
+          />
+        </div>
         <ImageDropzone setImages={setImages} />
         {images.length > 0 && (
           <ThumbnailsDND images={images} setImages={setImages} />
