c698475989f302f951694c0017ee73fbba399c76
Nitin Ranganath <nitinranganath@gmail.com>
2021-08-31 02:14:04 +0530

    Mutation to follow and unfollow an user

--- components/profile-page/ProfileHeader.jsx
+++ components/profile-page/ProfileHeader.jsx
@@ -1,5 +1,8 @@
+import axios from 'axios';
 import Link from 'next/link';
 import Image from 'next/image';
+import cookie from 'js-cookie';
+import { useMutation, useQueryClient } from 'react-query';
 import {
   UserAddIcon,
   CogIcon,
@@ -7,10 +10,37 @@ import {
   CheckCircleIcon,
 } from '@heroicons/react/outline';
 
+import baseURL from '../../utils/baseURL';
+
 const ProfileHeader = ({ profile, followers, following, user }) => {
   const isFollowing =
     user &&
     followers.filter((follower) => follower.user === user._id).length > 0;
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    async () => {
+      const { data } = await axios.post(
+        `${baseURL}/api/profile/follow/${profile.user._id}`,
+        {},
+        { headers: { Authorization: cookie.get('token') } }
+      );
+      return data;
+    },
+    {
+      onSuccess: (data) => {
+        const old = queryClient.getQueryData([
+          'profiles',
+          profile.user.username,
+        ]);
+        queryClient.setQueryData(['profiles', profile.user.username], {
+          ...old,
+          followers: data,
+        });
+      },
+    }
+  );
 
   return (
     <div className="flex justify-center px-8 py-8 md:py-12">
@@ -35,11 +65,17 @@ const ProfileHeader = ({ profile, followers, following, user }) => {
           </button>
         ) : user ? (
           isFollowing ? (
-            <button className="bg-pink-100 text-pink-600 text flex items-center font-semibold rounded-md px-3 py-2">
+            <button
+              onClick={() => mutation.mutate()}
+              className="bg-pink-100 text-pink-600 text flex items-center font-semibold rounded-md px-3 py-2"
+            >
               <CheckCircleIcon className="h-5 w-5 mr-2" /> Following
             </button>
           ) : (
-            <button className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2">
+            <button
+              onClick={() => mutation.mutate()}
+              className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2"
+            >
               <UserAddIcon className="h-5 w-5 mr-2" /> Follow
             </button>
           )
