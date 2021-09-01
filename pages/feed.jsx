fdeda2a047595034035a9edb570f2b647d35b316
Nitin Ranganath <nitinranganath@gmail.com>
2021-09-01 17:04:30 +0530

    Frontend implementation of feed page

--- pages/feed.jsx
+++ pages/feed.jsx
@@ -1,9 +1,37 @@
+import axios from 'axios';
+import cookie from 'js-cookie';
+import { parseCookies } from 'nookies';
+import { QueryClient, useQuery } from 'react-query';
+import { dehydrate } from 'react-query/hydration';
+
+import baseURL from '../utils/baseURL';
+import PostCard from '../components/PostCard';
+
+const getFeed = async (token) => {
+  const { data } = await axios.get(`${baseURL}/api/posts/feed`, {
+    headers: { Authorization: token },
+  });
+  return data;
+};
+
 const FeedPage = ({ user }) => {
+  const { data } = useQuery(['feed'], () => getFeed(cookie.get('token')));
+
   return (
-    <div>
-      <h1>{user.name}'s Feed</h1>
+    <div className="container mx-auto p-8 md:px-16 md:py-10 grid gap-5 place-items-center grid-cols-auto-fit">
+      {data.map((post) => (
+        <PostCard user={user} key={post._id} post={post} />
+      ))}
     </div>
   );
 };
+
+export async function getServerSideProps(ctx) {
+  const { token } = parseCookies(ctx);
+
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(['feed'], () => getFeed(token));
+  return { props: { dehydratedState: dehydrate(queryClient) } };
+}
 
 export default FeedPage;
