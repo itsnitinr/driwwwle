7a01714b32a4252998abd9395bf96ff3db03fa7b
Nitin Ranganath <nitinranganath@gmail.com>
2021-09-01 18:45:09 +0530

    Build notification model

---
+++ models/Notification.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const notificationSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  notifications: [
+    {
+      type: {
+        type: 'String',
+        enum: ['like', 'comment', 'follow'],
+      },
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+      post: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Post',
+      },
+      commentId: {
+        type: String,
+      },
+      text: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+});
+
+module.exports = mongoose.model('Notification', notificationSchema);
