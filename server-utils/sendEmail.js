245ce5a2420b3f28e60af8f5d0870ded0f8e2234
Nitin Ranganath <nitinranganath@gmail.com>
2021-09-07 22:52:11 +0530

    Switch to sendgrid for sending emails

--- server-utils/sendEmail.js
+++ server-utils/sendEmail.js
@@ -1,16 +1,19 @@
 const nodemailer = require('nodemailer');
+const sendgridTransport = require('nodemailer-sendgrid-transport');
+
+const sendgridOptions = {
+  auth: {
+    api_key: process.env.SENDGRID_API_KEY,
+  },
+};
 
 const sendEmail = async (options) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: process.env.SMTP_PORT,
-    auth: {
-      user: process.env.SMTP_USERNAME,
-      pass: process.env.SMTP_PASSWORD,
-    },
-  });
+  const transporter = nodemailer.createTransport(
+    sendgridTransport(sendgridOptions)
+  );
 
   await transporter.sendMail({
+    from: process.env.SENDER_EMAIL,
     to: options.to,
     subject: options.subject,
     text: options.text,
