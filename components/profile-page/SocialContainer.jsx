d7bc96e0cb42709160e285980f2c02cd17f22780
Nitin Ranganath <nitinranganath@gmail.com>
2021-09-15 02:40:45 +0530

    Add hrefs to social profile links

--- components/profile-page/SocialContainer.jsx
+++ components/profile-page/SocialContainer.jsx
@@ -11,34 +11,64 @@ const SocialContainer = ({ social }) => {
   return (
     <>
       {social?.github && (
-        <a className="flex items-center mb-2 text-gray-700">
+        <a
+          href={`https://github.com/${social?.github}`}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mb-2 text-gray-700 cursor-pointer hover:text-pink-600 transition"
+        >
           <AiFillGithub className="h-6 w-6 mr-2" /> <p>{social?.github}</p>
         </a>
       )}
       {social?.website && (
-        <a className="flex items-center mb-2 text-gray-700">
+        <a
+          href={`https://${social?.website}`}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mb-2 text-gray-700 cursor-pointer hover:text-pink-600 transition"
+        >
           <GoBrowser className="h-6 w-6 mr-2" /> <p>{social?.website}</p>
         </a>
       )}
       {social?.twitter && (
-        <a className="flex items-center mb-2 text-gray-700">
+        <a
+          href={`https://twitter.com/${social?.twitter}`}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mb-2 text-gray-700 cursor-pointer hover:text-pink-600 transition"
+        >
           <AiFillTwitterCircle className="h-6 w-6 mr-2" />{' '}
           <p>{social?.twitter}</p>
         </a>
       )}
       {social?.instagram && (
-        <a className="flex items-center mb-2 text-gray-700">
+        <a
+          href={`https://instagram.com/${social?.instagram}`}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mb-2 text-gray-700 cursor-pointer hover:text-pink-600 transition"
+        >
           <AiFillInstagram className="h-6 w-6 mr-2" />{' '}
           <p>{social?.instagram}</p>
         </a>
       )}
       {social?.linkedin && (
-        <a className="flex items-center mb-2 text-gray-700">
+        <a
+          href={`https://linkedin.com/in/${social?.linkedin}`}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mb-2 text-gray-700 cursor-pointer hover:text-pink-600 transition"
+        >
           <AiFillLinkedin className="h-6 w-6 mr-2" /> <p>{social?.linkedin}</p>
         </a>
       )}
       {social?.youtube && (
-        <a className="flex items-center mb-2 text-gray-700">
+        <a
+          href={`https://youtube.com/c/${social?.youtube}`}
+          target="_blank"
+          rel="noreferrer"
+          className="flex items-center mb-2 text-gray-700 cursor-pointer hover:text-pink-600 transition"
+        >
           <AiFillYoutube className="h-6 w-6 mr-2" /> <p>{social?.youtube}</p>
         </a>
       )}
