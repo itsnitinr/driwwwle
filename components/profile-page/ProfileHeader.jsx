6ad6bf496cf7bfb2bb8548656095e2b7c008e4b4
Nitin Ranganath <nitinranganath@gmail.com>
2021-09-14 17:05:31 +0530

    Build modal for display followers and following users

--- components/profile-page/ProfileHeader.jsx
+++ components/profile-page/ProfileHeader.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Link from 'next/link';
 import Image from 'next/image';
 import cookie from 'js-cookie';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 import {
   UserAddIcon,
@@ -10,6 +10,8 @@ import {
   LoginIcon,
   CheckCircleIcon,
 } from '@heroicons/react/outline';
+
+import FollowModal from '../../components/FollowModal';
 
 import baseURL from '../../utils/baseURL';
 
@@ -43,58 +45,87 @@ const ProfileHeader = ({ profile, followers, following, user }) => {
     }
   );
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [statsToShow, setStatsToShow] = useState('followers');
+
   useEffect(() => {
     document.title = `${profile.user.name}'s Profile on Driwwwle`;
   }, [profile]);
 
   return (
-    <div className="flex justify-center px-8 py-8 md:py-12">
-      <div>
-        <Image
-          className="rounded-full"
-          src={profile.user.profilePicUrl}
-          height={100}
-          width={100}
-        />
-      </div>
-      <div className="ml-5">
-        <h1 className="text-2xl mb-1 text-gray-800 font-bold">
-          {profile.user.name}
-        </h1>
-        <h3 className="mb-2">
-          {followers.length} Followers | {following.length} Following
-        </h3>
-        {user && user._id === profile.user._id ? (
-          <Link href="/settings">
-            <button className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2">
-              <CogIcon className="h-5 w-5 mr-2" /> Update
-            </button>
-          </Link>
-        ) : user ? (
-          isFollowing ? (
-            <button
-              onClick={() => mutation.mutate()}
-              className="bg-pink-100 text-pink-600 text flex items-center font-semibold rounded-md px-3 py-2"
+    <>
+      <div className="flex justify-center px-8 py-8 md:py-12">
+        <div>
+          <Image
+            className="rounded-full"
+            src={profile.user.profilePicUrl}
+            height={100}
+            width={100}
+          />
+        </div>
+        <div className="ml-5">
+          <h1 className="text-2xl mb-1 text-gray-800 font-bold">
+            {profile.user.name}
+          </h1>
+          <h3 className="mb-2">
+            <span
+              onClick={() => {
+                setStatsToShow('followers');
+                setModalOpen(true);
+              }}
+              className="hover:text-pink-600 cursor-pointer"
             >
-              <CheckCircleIcon className="h-5 w-5 mr-2" /> Following
-            </button>
+              {followers.length} Followers
+            </span>{' '}
+            |{' '}
+            <span
+              onClick={() => {
+                setStatsToShow('following');
+                setModalOpen(true);
+              }}
+              className="hover:text-pink-600 cursor-pointer"
+            >
+              {following.length} Following
+            </span>
+          </h3>
+          {user && user._id === profile.user._id ? (
+            <Link href="/settings">
+              <button className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2">
+                <CogIcon className="h-5 w-5 mr-2" /> Update
+              </button>
+            </Link>
+          ) : user ? (
+            isFollowing ? (
+              <button
+                onClick={() => mutation.mutate()}
+                className="bg-pink-100 text-pink-600 text flex items-center font-semibold rounded-md px-3 py-2"
+              >
+                <CheckCircleIcon className="h-5 w-5 mr-2" /> Following
+              </button>
+            ) : (
+              <button
+                onClick={() => mutation.mutate()}
+                className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2"
+              >
+                <UserAddIcon className="h-5 w-5 mr-2" /> Follow
+              </button>
+            )
           ) : (
-            <button
-              onClick={() => mutation.mutate()}
-              className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2"
-            >
-              <UserAddIcon className="h-5 w-5 mr-2" /> Follow
-            </button>
-          )
-        ) : (
-          <Link href="/login">
-            <button className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2">
-              <LoginIcon className="h-5 w-5 mr-2" /> Log In
-            </button>
-          </Link>
-        )}
+            <Link href="/login">
+              <button className="bg-gray-100 flex items-center font-semibold rounded-md px-3 py-2">
+                <LoginIcon className="h-5 w-5 mr-2" /> Log In
+              </button>
+            </Link>
+          )}
+        </div>
       </div>
-    </div>
+      <FollowModal
+        username={profile.user.username}
+        open={modalOpen}
+        setOpen={setModalOpen}
+        statsToShow={statsToShow}
+      />
+    </>
   );
 };
 
