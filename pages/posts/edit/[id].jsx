aa2d86eab24bb3f645ce3b24b761ce33d30a0b62
Nitin Ranganath <nitinranganath@gmail.com>
2021-10-04 21:01:19 +0530

    Setup backend API route for edit post and add mutation

--- pages/posts/edit/[id].jsx
+++ pages/posts/edit/[id].jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import cookie from 'js-cookie';
 import { useState } from 'react';
+import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
-import { useQuery, QueryClient } from 'react-query';
+import { useQuery, QueryClient, useMutation } from 'react-query';
 import { dehydrate } from 'react-query/hydration';
 import { AiOutlineLoading } from 'react-icons/ai';
 import {
@@ -10,7 +11,7 @@ import {
   TerminalIcon,
   LinkIcon,
   CodeIcon,
-  PlusCircleIcon,
+  PencilAltIcon,
 } from '@heroicons/react/outline';
 
 import PostInput from '../../../components/new-post/PostInput';
@@ -40,9 +41,49 @@ const EditPostPage = ({ user }) => {
   const [images, setImages] = useState(data?.images);
   const [isOriginalImages, setIsOriginalImages] = useState(true);
 
-  const onSubmit = (e) => {
+  const mutation = useMutation(
+    async (formdata) =>
+      await axios.put(`${baseURL}/api/posts/${id}`, formdata, {
+        headers: {
+          Authorization: cookie.get('token'),
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+  );
+
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log({ images, isOriginalImages });
+    const formdata = new FormData();
+
+    if (description.trim() === '') {
+      return toast.error('Please add a description');
+    }
+
+    formdata.append('title', title);
+    formdata.append('description', description);
+    formdata.append(
+      'techStack',
+      JSON.stringify(techStack.split(',').map((item) => item.trim()))
+    );
+    formdata.append('liveDemo', liveDemo);
+    formdata.append('sourceCode', sourceCode);
+    formdata.append('isOriginalImages', isOriginalImages);
+
+    if (isOriginalImages) {
+      formdata.append('originalImages', JSON.stringify(images));
+    } else {
+      for (const key of Object.keys(images)) {
+        formdata.append('images', images[key]);
+      }
+    }
+
+    try {
+      await mutation.mutateAsync(formdata);
+      toast.success('Your post has been successfully updated');
+      router.push(`/posts/${id}`);
+    } catch (err) {
+      toast.error(err.response?.data?.msg || 'Please recheck your inputs');
+    }
   };
 
   return (
@@ -114,7 +155,13 @@ const EditPostPage = ({ user }) => {
           <button
             type="submit"
             className="mt-8 flex items-center shadow w-full justify-center md:w-max ml-auto bg-pink-600 px-4 py-2 text-white font-semibold rounded-lg disabled:pointer-events-none disabled:opacity-50"
+            disabled={mutation.isLoading}
           >
+            {mutation.isLoading ? (
+              <AiOutlineLoading className="h-5 w-5 mr-2 animate-spin" />
+            ) : (
+              <PencilAltIcon className="h-5 w-5 mr-2" />
+            )}
             Edit Post
           </button>
         </form>
